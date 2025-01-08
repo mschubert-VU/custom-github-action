29a2e9794529a49fb9cddb050c11c75f64d6452f
Your Name <you@example.com>
2025-01-08 22:24:58 +0100

    add labels action

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import { getInput } from '@actions/core'
+import { getInput, setFailed } from '@actions/core'
+import { context, getOctokit } from '@actions/github'
 
-const name  = getInput('name')
+async function run() {
+  const token = getInput('github-token')
+  const label = getInput('label')
 
-console.log(`Hello, ${name}!`)
+  const octokit = getOctokit(token)
+  const pullRequest = context.payload.pull_request
+
+  try {
+    if (!pullRequest) {
+      throw new Error('This action can only be run on pullRequests')
+    }
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label]
+    })
+  } catch (error) {
+    setFailed((error as Error)?.message ?? 'Unknown error')
+  }
+}
+
+run()
